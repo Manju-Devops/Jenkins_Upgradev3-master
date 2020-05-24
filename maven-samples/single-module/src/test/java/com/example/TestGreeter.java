f554779b088fe0346caa275e7de70a3ba37f8263
Manje, Gowda <gowdamanju250@gmail.com>
2020-05-24 13:21:55 +0530

    archive Artifacts

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -33,7 +33,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you for DEVOPS tra";
+    String someone = " This is Archive artifacts";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
